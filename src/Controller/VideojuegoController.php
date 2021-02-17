9caaf41d1dc68065c95ef249f6d1635266b885ea
sguigom541 <2daasguigom21@ieslasfuentezuelas.com>
2021-02-17 14:09:31 +0100

    Subida de fotos de videojuegos

---
+++ src/Controller/VideojuegoController.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Videojuego;
+use App\Form\VideojuegoType;
+use App\Repository\VideojuegoRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Constraints\Date;
+
+/**
+ * @IsGranted("ROLE_ADMIN")
+ * @Route("/admin/videojuego")
+ */
+class VideojuegoController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_videojuego_index", methods={"GET"})
+     * @param VideojuegoRepository $videojuegoRepository
+     * @return Response
+     */
+    public function index(VideojuegoRepository $videojuegoRepository): Response
+    {
+        return $this->render('backend/videojuego/index.html.twig', [
+            'videojuegos' => $videojuegoRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="app_videojuego_new", methods={"GET","POST"})
+     * @param Request $request
+     * @return Response
+     */
+    public function new(Request $request): Response
+    {
+        $videojuego = new Videojuego();
+        $form = $this->createForm(VideojuegoType::class, $videojuego);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $imagenPrincipal = $form->get('imgPrincipal')->getData();
+            $imagenes=$form->get('imagenes')->getData();
+
+            if ($imagenPrincipal)
+            {
+                $fecha = new Date();
+                $fecha=date('Y-m-d');
+                try {
+
+                    $fotoFormateada = $fecha . '-' . trim($videojuego->getNombre(),"") . '-' . trim($videojuego->getPlataforma()->getNombre(),"") . '.' . $imagenPrincipal->guessExtension();
+                    $url=$this->getParameter('videojuego_directory').'/'.$videojuego->getNombre().'/'.'imagenPrincipal'.'/';
+                    $imagenPrincipal->move($url, $fotoFormateada);
+
+                } catch (FileException $e) {
+                    // ... handle exception if something happens during file upload
+                }
+                $videojuego->setImgPrincipal($fotoFormateada);
+            }
+            //si existe el array
+            if($imagenes)
+            {
+                $fecha = new Date();
+                $fecha=date('Y-m-d');
+                $arrayImagenes=array();
+                $url=$this->getParameter('videojuego_directory').'/'.$videojuego->getNombre().'/'.'imagenes'.'/';
+                for ($i=0;$i<count($imagenes);$i++)
+                {
+                    $imagenFormateada=$i.'-'.$fecha . '-' . trim($videojuego->getNombre(),"") . '-' . trim($videojuego->getPlataforma()->getNombre(),"") . '.' . $imagenes[$i]->guessExtension();
+                    $imagenes[$i]->move($url,$imagenFormateada);
+                    array_push($arrayImagenes,$imagenFormateada);
+
+                }
+
+                $videojuego->setImagenes($arrayImagenes);
+            }
+
+
+
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($videojuego);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_videojuego_index');
+        }
+
+        return $this->render('backend/videojuego/new.html.twig', [
+            'videojuego' => $videojuego,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="app_videojuego_show", methods={"GET"})
+     */
+    public function show(Videojuego $videojuego): Response
+    {
+        return $this->render('backend/videojuego/show.html.twig', [
+            'videojuego' => $videojuego,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="app_videojuego_edit", methods={"GET","POST"})
+     * @param Request $request
+     * @param Videojuego $videojuego
+     * @return Response
+     */
+    public function edit(Request $request, Videojuego $videojuego): Response
+    {
+        $form = $this->createForm(VideojuegoType::class, $videojuego);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('app_videojuego_index');
+        }
+
+        return $this->render('backend/videojuego/edit.html.twig', [
+            'videojuego' => $videojuego,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="app_videojuego_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Videojuego $videojuego): Response
+    {
+        if ($this->isCsrfTokenValid('delete' . $videojuego->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($videojuego);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_videojuego_index');
+    }
+}
---
+++ src/Form/VideojuegoType.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Categoria;
+use App\Entity\Plataforma;
+use App\Entity\Videojuego;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
+
+class VideojuegoType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nombre')
+            ->add('lanzamiento', DateType::class, [
+                'widget' => 'single_text',
+                'format' => 'yyyy-MM-dd'
+            ])
+            ->add('fechaHoraEntrada', DateTimeType::class, [
+                'widget' => 'single_text',
+            ])
+            ->add('precio')
+            ->add('descuento')
+            ->add('stock')
+            ->add('descripcion')
+            ->add('imgPrincipal', FileType::class, [
+                'label' => 'Imagen (JPG, PNG)',
+
+                // unmapped means that this field is not associated to any entity property
+                'mapped' => false,
+
+                // make it optional so you don't have to re-upload the PDF file
+                // every time you edit the Product details
+                'required' => false,
+
+                // unmapped fields can't define their validation using annotations
+                // in the associated entity, so you can use the PHP constraint classes
+                'constraints' => [
+                    new File([
+                        'maxSize' => '1024k',
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/webp',
+                            'image/png',
+                            'image/jpg'
+                        ],
+                        'mimeTypesMessage' => 'Inserte un archivo extension jpg/png/jpeg/wepb',
+                    ])
+                ],
+            ])
+            ->add('imagenes', FileType::class, [
+                'label' => 'Imagenes (JPG, PNG)',
+                'multiple' => true,
+                'required' => false,
+
+            ])
+            ->add('categoria', EntityType::class, [
+                // looks for choices from this entity
+                'class' => Categoria::class,
+
+                // uses the User.username property as the visible option string
+                'choice_label' => 'nombre',
+
+                // used to render a select box, check boxes or radios
+                // 'multiple' => true,
+                // 'expanded' => true,
+            ])
+            ->add('plataforma', EntityType::class, [
+                // looks for choices from this entity
+                'class' => Plataforma::class,
+
+                // uses the User.username property as the visible option string
+                'choice_label' => 'nombre',
+
+                // used to render a select box, check boxes or radios
+                // 'multiple' => true,
+                // 'expanded' => true,
+            ]);;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Videojuego::class,
+        ]);
+    }
+}
