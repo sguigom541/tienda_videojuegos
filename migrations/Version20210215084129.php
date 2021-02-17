0b6806e0ae5a280fedbde13ca2187fbf7a8ca88f
sguigom541 <2daasguigom21@ieslasfuentezuelas.com>
2021-02-17 14:19:46 +0100

    Actualización del proyecto

---
+++ migrations/Version20210215084129.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210215084129 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE videojuego ADD img_principal LONGTEXT NOT NULL, ADD imagenes LONGTEXT DEFAULT NULL COMMENT \'(DC2Type:array)\'');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE videojuego DROP img_principal, DROP imagenes');
+    }
+}
---
+++ public/backend/js/forms.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $(".datepicker").datepicker({
+        showAnim: 'slideDown'
+    });
+    $(".datetimepicker").datetimepicker({
+       showAnim: 'slide'
+    });
+})
